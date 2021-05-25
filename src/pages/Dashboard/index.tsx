9d5a9c3d210ea150ee1bd4eff1e444322d80e02b
Gilvan K <gilvam.izidio.dev@gmail.com>
2021-05-25 14:21:58 -0300

    Feat:<Dashboard>Concluido pagina de pesquisa com consumo da API -Feito tratamento de erros para repositorios inexistentes e quando informado em branco

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,43 +1,96 @@
-import React from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronsRight } from 'react-icons/fi';
-import { Title, Form, Repositories } from './styles';
+import { Link } from 'react-router-dom';
+import api from '../../services/api';
+import { Title, Form, Repositories, Error } from './styles';
 import gitexplorer from '../../assets/gitexplorer.svg';
+import Repository from '../Repository';
+
+interface Repository {
+    full_name: string;
+    description: string;
+    owner: {
+        login: string;
+        avatar_url: string;
+    };
+}
 
 const Dashboard: React.FC = () => {
+    const [newRepo, setNewRepo] = useState('');
+    const [inputError, setInputError] = useState('');
+
+    const [repositories, setRepositories] = useState<Repository[]>(() => {
+        const storagedRepositories = localStorage.getItem(
+            '@githubExplorer:repositories',
+        );
+        if (storagedRepositories) {
+            return JSON.parse(storagedRepositories);
+        }
+        return [];
+    });
+
+    useEffect(() => {
+        localStorage.setItem(
+            '@githubExplorer:repositories',
+            JSON.stringify(repositories),
+        );
+    }, [repositories]);
+
+    async function handleAddRepository(
+        event: FormEvent<HTMLFormElement>,
+    ): Promise<void> {
+        event.preventDefault();
+
+        if (!newRepo) {
+            setInputError('Digite o autor/nome do repositório');
+            return;
+        }
+
+        try {
+            const response = await api.get<Repository>(`repos/${newRepo}`);
+            const repository = response.data;
+
+            setRepositories([...repositories, repository]);
+            setNewRepo('');
+            setInputError('');
+        } catch (err) {
+            setInputError('Erro na busca por esse repositório');
+        }
+    }
+
     return (
         <>
             <img src={gitexplorer} alt="GithubExplorer" />
             <Title>Explore respositórios no Github</Title>
 
-            <Form>
-                <input placeholder="Digite o nome do repositório" />
+            <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+                <input
+                    value={newRepo}
+                    onChange={(e) => setNewRepo(e.target.value)}
+                    placeholder="Digite o nome do repositório"
+                />
                 <button type="submit">Pesquisar</button>
             </Form>
 
-            <Repositories>
-                <a href="Teste">
-                    <img
-                        src="https://avatars.githubusercontent.com/u/62617637?v=4"
-                        alt="imageProfile"
-                    />
-                    <div>
-                        <strong>PlantManager</strong>
-                        <p>repo referente nlw 5</p>
-                    </div>
-                    <FiChevronsRight size={20} />
-                </a>
+            {inputError && <Error>{inputError}</Error>}
 
-                <a href="Teste">
-                    <img
-                        src="https://avatars.githubusercontent.com/u/62617637?v=4"
-                        alt="imageProfile"
-                    />
-                    <div>
-                        <strong>PlantManager</strong>
-                        <p>repo referente nlw 5</p>
-                    </div>
-                    <FiChevronsRight size={20} />
-                </a>
+            <Repositories>
+                {repositories.map((repository) => (
+                    <Link
+                        key={repository.full_name}
+                        to={`/repositories/${repository.full_name}`}
+                    >
+                        <img
+                            src={repository.owner.avatar_url}
+                            alt={repository.owner.login}
+                        />
+                        <div>
+                            <strong>{repository.full_name}</strong>
+                            <p>{repository.description}</p>
+                        </div>
+                        <FiChevronsRight size={20} />
+                    </Link>
+                ))}
             </Repositories>
         </>
     );
